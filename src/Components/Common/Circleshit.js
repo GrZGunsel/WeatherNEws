c5fac063550c3651660d2782bb3882956cde4a7f
kshitz moktan <you@example.com>
2021-10-03 12:33:26 +0545

    sad

--- src/Components/Common/Circleshit.js
+++ src/Components/Common/Circleshit.js
@@ -4,11 +4,9 @@ const Circleshit = () => {
   return (
     <div className="design">
       <div className="circle1"></div>
-      <div className="circle2"></div>
+
       <div className="circle3"></div>
       <div className="circle4"></div>
-      <div className="circle5"></div>
-      <div className="circle6"></div>
     </div>
   );
 };
